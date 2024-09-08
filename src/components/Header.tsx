6638fb1dfaa02f2db41fbe7c0eb6c2f0e1ac7727
Duosi-Dai <daiduosi1999@gmail.com>
2024-09-08 10:53:22 +0200

    added social media link on header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,5 @@
 import profile from "../assets/profile.png";
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 
 const Header = () => {
   return (
@@ -6,13 +7,22 @@ const Header = () => {
       <div className="header">
         <div className="header__title">
           <h1>
-            <span className="highlight">Name</span>
-            : I am a
+            Hi There,
             <br />
-            <span className="highlight">Career field</span>
+            I'm <span className="highlight">Duosi Dai</span>
+            <br />
+            {/* <span className="highlight">Career field</span> */}
           </h1>
           <h4>Self-introduction Pitch</h4>
           <button className="btn--text">More about Me</button>
+          <div className="contactIcon">
+            <a href="https://github.com/Duosi-Dai">
+              <FaGithub className="icon" />
+            </a>
+            <a href="https://www.linkedin.com/in/duosi-dai-964218225/">
+              <FaLinkedin />
+            </a>
+          </div>
           <img src={profile} className="header__img" alt="Nothing" />
         </div>
       </div>
