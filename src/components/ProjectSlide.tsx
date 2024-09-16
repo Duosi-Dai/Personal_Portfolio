d9823b4fa446acd67ad82d93cc8d8a26966da6ce
Duosi-Dai <daiduosi1999@gmail.com>
2024-09-16 19:49:15 +0200

    Refinement on Project session

--- src/components/ProjectSlide.tsx
+++ src/components/ProjectSlide.tsx
@@ -6,43 +6,72 @@ interface ProjectSlideProps {
   pdfLink?: string;
   repoLink?: string;
   blogLink?: string;
+  img?: string;
 }
 
-const ProjectSlide = ({  
+const ProjectSlide = ({
   title,
   description,
   date,
   videoLink,
   pdfLink,
   repoLink,
-  blogLink, }: ProjectSlideProps) => {
+  blogLink,
+  img,
+}: ProjectSlideProps) => {
   return (
     <div className="slide">
       <div className="project">
-        <h2 className="project__name">{title}</h2>
-        <p className="project__description">{description}</p>
+        <h2 className="project__name highlight">{title}</h2>
         <p className="project__date">{date}</p>
-        <div className="project__links">
-          {videoLink && (
-            <a href={videoLink} className="project__link" target="_blank" rel="noopener noreferrer">
-              Video Demo
-            </a>
-          )}
-          {pdfLink && (
-            <a href={pdfLink} className="project__link" target="_blank" rel="noopener noreferrer">
-              Thesis PDF
-            </a>
-          )}
-          {repoLink && (
-            <a href={repoLink} className="project__link" target="_blank" rel="noopener noreferrer">
-              GitHub Repository
-            </a>
-          )}
-          {blogLink && blogLink !== 'placeholder' && (
-            <a href={blogLink} className="project__link" target="_blank" rel="noopener noreferrer">
-              Blog
+
+        <p className="project__description">{description}</p>
+        <div className="project__linkNImg">
+          <div className="project__img">
+            <img src={img} />
+          </div>
+          <div className="project__links">
+            {videoLink && (
+              <a
+                href={videoLink}
+                className="project__link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Video Demo
               </a>
-          )}
+            )}
+            {pdfLink && (
+              <a
+                href={pdfLink}
+                className="project__link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Thesis PDF
+              </a>
+            )}
+            {repoLink && (
+              <a
+                href={repoLink}
+                className="project__link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                GitHub Repository
+              </a>
+            )}
+            {blogLink && blogLink !== 'placeholder' && (
+              <a
+                href={blogLink}
+                className="project__link"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Blog
+              </a>
+            )}
+          </div>
         </div>
       </div>
     </div>
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,46 +1,57 @@
 import { useState } from 'react';
 import ProjectSlide from '../components/ProjectSlide';
+import emoCanvas from '../assets/Emocanvas.png';
+import bThesis from '../assets/BachelorThesis.png';
+import bookKeeper from '../assets/bookKeeper.png';
+import rpgGame from '../assets/RPGgame.png';
+import simFluid from '../assets/SimFluid.png';
 
 const Projects = () => {
   const projects = [
     {
-      title: 'EmoCanvas',
+      title: 'LLM multimodal interface: EmoCanvas',
       description:
         'EmoCanvas is an innovative multimodal interface designed to enhance user interaction by capturing input from various modalities and delivering highly personalized, emotionally resonant output. By leveraging the power of Large Language Models (LLMs), EmoCanvas transforms user speech into vivid, custom-generated images, interpreting facial expressions and emotions in real-time.',
       date: 'Oct 2023 - Jan 2024',
       videoLink: 'https://www.youtube.com/watch?v=FJzWE-XxcVw',
+      img: emoCanvas,
     },
     {
-      title: 'LoRa Radio Performance Analysis',
+      title: 'Internet of Things: LoRa Radio Performance Analysis',
       description:
         'This thesis is dedicated to measuring the communication performance of LoRa technology in an indoor environment, assessing transmission parameters such as SF, BW, and PL, and analyzing its effectiveness as a solution for indoor IoT applications.',
       date: 'Jan 2023 - Jun 2023',
       pdfLink:
         'https://www.diva-portal.org/smash/get/diva2:1792968/FULLTEXT01.pdf',
+      img: bThesis,
     },
+
     {
-      title: 'Book-Keeper',
+      title: 'Dynamic Web: Book-Keeper',
       description:
         'This is a live bookkeeping software website allowing users to store and manage bookkeeping records, including income and expenses, with real-time editing, price comparisons, and product tracking features.',
       date: 'Oct 2021 - Jan 2023',
       repoLink: 'https://github.com/Dynamic-Web-Project/Book-keeper',
+      img: bookKeeper,
     },
     {
-      title: 'Project Lost in Translatopia',
+      title: 'RPG-Game: Lost in Translatopia',
       description:
         'An RPG adventure game where the player is a non-human animal exploring an open-world with different language regions.',
       date: 'Mar 2024 - Jun 2024',
       videoLink: 'https://youtu.be/xXCMSbCrndI',
       blogLink: 'https://lost-in-translatopia.webflow.io/',
+      img: rpgGame,
     },
     {
       title:
-        'Real-Time Hand-Tracking Interaction with GPU-Based SPH Fluid in VR',
+        'Graphic Design: Hand-Tracking interaction with Simulated Fluid in VR',
       description:
         'Explores real-time hand-tracking interactions with GPU-based fluid simulation in VR, simulating realistic fluid behaviors using Unity and the Meta XR SDK for VR development.',
       date: 'Aug 2023 - Present',
       videoLink: 'https://www.youtube.com/watch?v=GQpZb-ERgT8',
       blogLink: 'https://interactive-sph-fluidsim-ingpu.webflow.io/',
+      img: simFluid,
     },
   ];
 
@@ -72,6 +83,7 @@ const Projects = () => {
             pdfLink={projects[activeSlide].pdfLink}
             repoLink={projects[activeSlide].repoLink}
             blogLink={projects[activeSlide].blogLink}
+            img={projects[activeSlide].img}
           />
 
           <button className="slider__btn slider__btn--left" onClick={prevSlide}>
